1d5510acb0f82cb603a1836c32d63e2f5fa93b4d
Bhavya <121705262+bhavya681@users.noreply.github.com>
2024-06-07 17:56:51 +0530

    Update SkillText.tsx

--- components/sub/SkillText.tsx
+++ components/sub/SkillText.tsx
@@ -13,7 +13,7 @@ const SkillText = () => {
         >
           <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
           <h1 className="Welcome-text text-[13px]">
-          Enhance Your Development Journey: Unleashing the Power of MERN Stack Mastery!"
+          Enhance Your Development Journey:  &quot;Unleashing the Power of MERN Stack Mastery!&quot;
           </h1>
         </motion.div>
         <motion.div
