41f5110e07328653c369b56e1b8963e140a1eee1
Bhavya <121705262+bhavya681@users.noreply.github.com>
2024-06-12 16:58:48 +0530

    Update HeroContent.tsx

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -1,3 +1,4 @@
+/*
 "use client";
 
 import React from "react";
@@ -71,3 +72,79 @@ const HeroContent = () => {
 };
 
 export default HeroContent;
+*/
+
+import React from "react";
+import { motion } from "framer-motion";
+import {
+  slideInFromLeft,
+  slideInFromRight,
+  slideInFromTop,
+} from "@/utils/motion";
+import { SparklesIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
+
+const HeroContent = () => {
+  return (
+    <motion.div
+      initial="hidden"
+      animate="visible"
+      className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]"
+    >
+      <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
+        <motion.div
+          variants={slideInFromTop}
+          className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
+        >
+          <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
+          <h1 className="Welcome-text text-[13px]">
+            Mernstack Developer Portfolio
+          </h1>
+        </motion.div>
+
+        <motion.div
+          variants={slideInFromLeft(0.5)}
+          className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
+        >
+          <span>
+            Connect with me, a MERN stack web developer, to bring your vision to
+            life!
+          </span>
+        </motion.div>
+
+        <motion.p
+          variants={slideInFromLeft(0.8)}
+          className="text-lg text-gray-400 my-5 max-w-[600px]"
+        >
+          I&apos;m a MERN Stack Developer proficient in Website, Mobile, and Software
+          Development. Explore my portfolio to see my projects and skills in
+          action!
+        </motion.p>
+        <motion.a
+          variants={slideInFromLeft(1)}
+          href="https://www.geeksforgeeks.org/mern-stack/"
+          className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
+        >
+          Learn More!
+        </motion.a>
+      </div>
+
+      <motion.div
+        variants={slideInFromRight(0.8)}
+        className="w-full h-full flex justify-center items-center"
+      >
+        <div className="rounded-full overflow-hidden w-64 h-64">
+          <Image
+            src="https://ibb.co/FHY1nw1" // Replace this with your developer image
+            alt="Developer"
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+      </motion.div>
+    </motion.div>
+  );
+};
+
+export default HeroContent;
+
